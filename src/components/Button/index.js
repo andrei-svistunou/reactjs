303b4b44a6beb7e05db23afcf1aae5f03bf7e906
Andrei_Svistunou <andrei_svistunou@epam.com>
2018-06-29 17:31:36 +0300

    Added storybook(does not work), try use flow and immutablejs, airbnb was added

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,16 +1,17 @@
+// @flow
+
 import React from 'react';
-import PropTypes from 'prop-types';
 import './index.css';
 
-const Button = props => (
+type ButtonProps = {
+  value: string,
+  classes: string,
+  onClick: () => {}
+}
+
+const Button = (props: ButtonProps) => (
   <input type="button" className={`btn ${props.classes} upper-text`} value={props.value} onClick={props.onClick}/>
 );
-
-Button.propTypes = {
-  value: PropTypes.string.isRequired,
-  classes: PropTypes.string,
-  onClick: PropTypes.func.isRequired
-};
 
 Button.defaultProps = {
   classes: '',
